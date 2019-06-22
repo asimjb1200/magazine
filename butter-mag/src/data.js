6fe4500fb5a23cf2621deafe2f033b535ab0fcd4
asimjb12 <44182316+asimjb12@users.noreply.github.com>
2019-06-22 14:15:58 -0500

    populating the data store testing later

---
+++ butter-mag/src/data.js
@@ -0,0 +1,11 @@
+import snippet from './newsImages/react.png'
+export const Articles =[
+    {
+        id: 1,
+        date: '01-20-2019',
+        author: 'Asim',
+        headline: 'First React App',
+        images: [snippet],
+        content: 'Testing out my very first context api',
+    }
+]
