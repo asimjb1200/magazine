6f72878aaf8335aac8f64c21ac2985ab317c961b
asimjb12 <44182316+asimjb12@users.noreply.github.com>
2019-07-11 19:41:27 -0500

    redoing sports section

--- butter-mag/src/components/SportSnip.js
+++ butter-mag/src/components/SportSnip.js
@@ -3,17 +3,24 @@ import React, { Component } from 'react'
 export class SportSnip extends Component {
     
     render() {
-        const {id, videoPath, home, away, headline} = this.props.article;
+        const {id, sport, date, videoPath, home, away, headline} = this.props.article;
         const VIDEO_ID = videoPath.substring(32);
-        console.log(VIDEO_ID);
+        const sportDate = date.substring(0, 10);
+        
         return (
-            <div className="card" style={{width: '20rem', height: '15rem'}}>
-                <div className="card-img-top youtube-player" data-id={VIDEO_ID}>
-                    
+            <div className="card" style={{width: '20rem', height: '15rem', backgroundColor: 'gold', fontFamily: 'Pacifico'}}>
+                <div className="card-header ">{sport}</div>
+                <ul className="list-group list-group-flush text-center" >
+                    <li className="list-group-item" style={{color: 'gold', backgroundColor: 'black'}}>{home}</li>
+                    <li className="list-group-item" style={{color: 'gold', backgroundColor: 'black'}}>VS</li>
+                    <li className="list-group-item" style={{color: 'gold', backgroundColor: 'black'}}>{away}</li>
+                </ul>
+                <div className="card-footer">
+                    {sportDate}
                 </div>
             </div>
         )
     }
 }
 
-export default SportSnip
+export default SportSnip;
